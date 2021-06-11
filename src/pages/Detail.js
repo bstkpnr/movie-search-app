7f60ced3d28d1b01245fc4735795eb1aa7b476f3
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-06-11 10:04:35 +0300

    first commit

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -9,8 +9,9 @@ import '../styles/Detail.css';
 const Detail = () => {
   // React Router'daki dinamik URL kısmını almalıyız (useParams)
   const { showDetail, selectedMovie } = useContext(MovieContext);
+  const {id}=useParams();
   useEffect(() => {
-    showDetail('tt1201607'); // useParams ile alınan film id'sini kullanınız. 
+    showDetail(id); // useParams ile alınan film id'sini kullanınız. 
   }, []);
 
   return (
